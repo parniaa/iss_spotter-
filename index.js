dd35fe7e1df4776f3104c319c87a41556b3bce27
paarmin <parniaa@gmail.com>
2021-01-14 17:24:33 -0500

    ssA

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
+
   console.log('It worked! Returned IP:' , ip);
 });
+fetchCoordsByIP('99.241.180.255', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
 
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
 
